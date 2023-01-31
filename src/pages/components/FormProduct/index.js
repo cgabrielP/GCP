b2dd26802d2762da3f01b3a723eeddcee1cfe8ff
Carlos Peña <cgabriel2303@gmail.com>
2023-01-31 17:26:31 -0300

    add: cloudinary

--- src/pages/components/FormProduct/index.js
+++ src/pages/components/FormProduct/index.js
@@ -1,4 +1,4 @@
-import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Box, Button, Checkbox, Flex, FormControl, FormLabel, Heading, Input, Link, Stack, useColorModeValue } from '@chakra-ui/react'
 import axios from 'axios'
 import { useRouter } from 'next/router'
 import React from 'react'
@@ -24,25 +24,47 @@ const FormProduct = () => {
     return (
         <form onSubmit={handleSubmit}>
             <FormControl >
-                <FormLabel>Nombre</FormLabel>
-                <Input type='text' name='name' onChange={handleInputChange} />
+                <Flex
+                    minH={'100vh'}
+                    bg={useColorModeValue('gray.50', 'gray.800')}>
+                    <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                        <Stack align={'center'}>
+                            <Heading fontSize={'4xl'}>Post a product</Heading>
+                        </Stack>
+                        <Box
+                            rounded={'lg'}
+                            bg={useColorModeValue('white', 'gray.700')}
+                            boxShadow={'lg'}
+                            p={8}>
+                            <Stack spacing={4}>
+                                <FormLabel>Nombre</FormLabel>
+                                <Input type='text' name='name' onChange={handleInputChange} />
+                                <FormLabel>Description</FormLabel>
+                                <Input type="text" name='description' onChange={handleInputChange} />
+                                <FormLabel>Image</FormLabel>
+                                <Input type='file' name='url' onChange={handleInputChange} />
 
-                <FormLabel>Description</FormLabel>
-                <Input type='text' name='description' onChange={handleInputChange} />
+                                <FormLabel>Price</FormLabel>
+                                <Input type='text' name='price' onChange={handleInputChange} />
+                                <Stack spacing={10}>
 
-                <FormLabel>Image</FormLabel>
-                <Input type='file' name='url' onChange={handleInputChange} />
+                                    <Button
+                                        type='submit'
 
-                <FormLabel>Price</FormLabel>
-                <Input type='text' name='price' onChange={handleInputChange} />
+                                        bg={'blue.400'}
+                                        color={'white'}
+                                        _hover={{
+                                            bg: 'blue.500',
+                                        }}>
+                                        Post
+                                    </Button>
+                                </Stack>
+                            </Stack>
+                        </Box>
+                    </Stack>
+                </Flex>
 
-                <Button
-                    mt={4}
-                    colorScheme='teal'
-                    type='submit'
-                >
-                    Submit
-                </Button>
+               
             </FormControl>
         </form>
 
--- src/pages/prueba/index.js
+++ src/pages/prueba/index.js
@@ -1,9 +1,23 @@
+import { CldUploadButton } from 'next-cloudinary';
 import React from 'react'
 import FormProduct from '../components/FormProduct'
 
 const prueba = () => {
   return (
-    <FormProduct/>
+    <>
+      <CldUploadButton
+        className={styles.button}
+        onUpload={(error, result, widget) => {
+          setResource(result?.info); // Updating local state with asset details
+          widget.close(); // Close widget immediately after successful upload
+        }}
+        signatureEndpoint="/api/sign-cloudinary-params"
+        uploadPreset="next-cloudinary-signed"
+      >
+        Upload to Cloudinary
+      </CldUploadButton>
+      <FormProduct />
+    </>
   )
 }
 
